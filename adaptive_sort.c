c21a1fa9ca00e2e615812fe7ed6bdbec2bb0ea97
Tiana Itokiana Andrianjaka Fenosoa <tiana-an@student.42antananarivo.mg>
2026-02-23 17:48:53 +0300

    Adaptive is simple because its explanation is on the subjects

--- adaptive_sort.c
+++ adaptive_sort.c
@@ -6,7 +6,7 @@
 /*   By: trakotoz <trakotoz@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 16:47:01 by trakotoz          #+#    #+#             */
-/*   Updated: 2026/02/23 16:58:45 by trakotoz         ###   ########.fr       */
+/*   Updated: 2026/02/23 17:44:35 by tiana-an         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 
 void	adaptive_sort(t_list **stack_a, t_list **stack_b, char **all_commands)
 {
+    int disorder;
+
 	if (!stack_a || !*stack_a)
 		error();
-	int size = ft_lstsize(*stack_a);
-	if (size <= 10)
+    disorder = compute_disorder(stack_a);
+	if (disorder < 200)
 		minmax_sort(stack_a, stack_b, all_commands);
-	if (size <= 360)
+	if (200 <= disorder && disorder < 500)
 		chunk_based_sort(stack_a, stack_b, all_commands);
-	if (size > 360)
+	if (disorder >= 500)
 		radix_lsd_sort(stack_a, stack_b, all_commands);
 }
