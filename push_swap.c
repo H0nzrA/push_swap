86e152db511d29fb459488ab31c6c72647b9822b
Teddy Andrianina Rakotozafy <trakotoz@student.42antananarivo.mg>
2026-02-23 13:45:21 +0300

    Benchmark version 3.0 : adding bench printing when the list is already sorted

--- push_swap.c
+++ push_swap.c
@@ -6,12 +6,20 @@
 /*   By: tiana-an <tiana-an@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 09:07:41 by trakotoz          #+#    #+#             */
-/*   Updated: 2026/02/23 09:02:18 by trakotoz         ###   ########.fr       */
+/*   Updated: 2026/02/23 12:58:47 by trakotoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf/libft/libft.h"
 #include "push_swap.h"
+
+static void print_bench_sorted(t_strat strategy, int bench)
+{
+	if (!bench)
+		return ;
+	char *all_commands = "";
+	print_command(all_commands, bench, 0, strategy);
+}
 
 void	start_algo(t_list **list, t_strat strategy, int bench)
 {
@@ -23,7 +31,10 @@ void	start_algo(t_list **list, t_strat strategy, int bench)
 	all_commands = NULL;
 	disorder = compute_disorder(list);
 	if (disorder == 0)
+	{
+		print_bench_sorted(strategy, bench);
 		return ;
+	}
 	if (strategy == SIMPLE)
 		minmax_sort(list, &stack_b, &all_commands);
 	if (strategy == COMPLEX)
