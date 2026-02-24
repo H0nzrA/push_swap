b76da5ef90dcd622ff2d7c44b3aa4ab772e58d11
Tiana Itokiana Andrianjaka Fenosoa <tiana-an@student.42antananarivo.mg>
2026-02-24 11:21:37 +0300

    Petite modification

--- adaptive_sort.c
+++ adaptive_sort.c
@@ -6,7 +6,7 @@
 /*   By: trakotoz <trakotoz@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 16:47:01 by trakotoz          #+#    #+#             */
-/*   Updated: 2026/02/23 17:44:35 by tiana-an         ###   ########.fr       */
+/*   Updated: 2026/02/24 08:43:07 by tiana-an         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ void	adaptive_sort(t_list **stack_a, t_list **stack_b, char **all_commands)
 	if (!stack_a || !*stack_a)
 		error();
     disorder = compute_disorder(stack_a);
-	if (disorder < 200)
+	if (disorder < 2000)
 		minmax_sort(stack_a, stack_b, all_commands);
-	if (200 <= disorder && disorder < 500)
+	if (2000 <= disorder && disorder < 5000)
 		chunk_based_sort(stack_a, stack_b, all_commands);
-	if (disorder >= 500)
+	if (disorder >= 5000)
 		radix_lsd_sort(stack_a, stack_b, all_commands);
 }
